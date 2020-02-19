01c3acfebe9b2d236a135a0433077c4296eabdba
xueyaorong <axuexiaorong@163.com>
2020-02-19 11:54:35 +0800

    2.18

--- nm-framework-common/src/main/java/com/ningmeng/framework/model/response/CommonCode.java
+++ nm-framework-common/src/main/java/com/ningmeng/framework/model/response/CommonCode.java
@@ -10,6 +10,7 @@ public enum CommonCode implements ResultCode{
     FAIL(false,11111,"操作失败！"),
     UNAUTHENTICATED(false,10001,"此操作需要登陆系统！"),
     UNAUTHORISE(false,10002,"权限不足，无权操作！"),
+    INCALID_PARAM(false,10003,"非法参数！"),
     SERVER_ERROR(false,99999,"抱歉，系统繁忙，请稍后重试！"),
     HTTP_ERROR(false,123123,"抱歉，请求参数异常，请稍后重试！");
 //    private static ImmutableMap<Integer, CommonCode> codes ;
