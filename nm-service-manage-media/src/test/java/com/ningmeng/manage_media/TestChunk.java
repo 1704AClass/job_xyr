a4506a04f61a9101f2af33d6e403d06d0ac1d5d9
xueyaorong <axuexiaorong@163.com>
2020-03-03 11:26:05 +0800

    3.2

---
+++ nm-service-manage-media/src/test/java/com/ningmeng/manage_media/TestChunk.java
@@ -0,0 +1,57 @@
+package com.ningmeng.manage_media;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class TestChunk {
+
+    //测试文件分块方法
+    @Test
+    public void testChunk() throws IOException {
+        //源文件地址
+        File sourceFile = new File("D:/video/haicaowu.mp4");
+        // File sourceFile = new File("d:/logo.png");
+        String chunkPath = "D:/video/chunk";
+        File chunkFolder = new File(chunkPath);
+        if(!chunkFolder.exists()){
+            //不存在创建目录
+            chunkFolder.mkdirs();
+        }
+        //设置分块大小 以kb为单位
+        long chunkSize = 1024*1024*1;
+        //得到块文件个数 应该采用向上转型
+        long chunkNum = (long) Math.ceil(sourceFile.length() * 1.0 / chunkSize );
+        if(chunkNum<=0){
+            chunkNum = 1;
+        }
+        //缓冲区大小
+        byte[] b = new byte[1024];
+        //使用RandomAccessFile访问文件
+        RandomAccessFile raf_read = new RandomAccessFile(sourceFile, "r");
+        //分块
+        for(int i=0;i<chunkNum;i++){
+            //创建分块文件
+            File file = new File(chunkPath+i);
+            boolean newFile = file.createNewFile();
+            if(newFile){
+                //向分块文件中写数据
+                RandomAccessFile raf_write = new RandomAccessFile(file, "rw");
+                int len = -1;
+                while((len = raf_read.read(b))!=-1){
+                    raf_write.write(b,0,len);
+                    if(file.length()>chunkSize){
+                        break;
+                    }
+                }
+                raf_write.close();
+            }
+        }
+        raf_read.close();
+    }
+
+
+
+}
---
+++ nm-service-manage-media/src/test/java/com/ningmeng/manage_media/TestMerge.java
@@ -0,0 +1,55 @@
+package com.ningmeng.manage_media;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.*;
+
+public class TestMerge {
+
+    //测试文件合并方法
+    @Test
+    public void testMerge() throws IOException {
+        //块文件目录
+        File chunkFolder = new File("D:/video/chunk");
+        //合并文件
+        File mergeFile = new File("D:/video/haicaowu.mp4");
+        if(mergeFile.exists()){
+            mergeFile.delete();
+        }
+        //创建新的合并文件
+        mergeFile.createNewFile();
+        //用于写文件
+        RandomAccessFile raf_write = new RandomAccessFile(mergeFile, "rw");
+        //指针指向文件顶端
+        raf_write.seek(0);
+        //缓冲区
+        byte[] b = new byte[1024];
+        //分块列表
+        File[] fileArray = chunkFolder.listFiles();
+        // 转成集合，便于排序
+        List<File> fileList = new ArrayList<File>(Arrays.asList(fileArray));
+        // 从小到大排序
+        Collections.sort(fileList, new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                if (Integer.parseInt(o1.getName()) < Integer.parseInt(o2.getName())) {
+                    return -1;
+                }
+                return 1;
+            }
+        });
+        //合并文件
+        for(File chunkFile:fileList){
+            RandomAccessFile raf_read = new RandomAccessFile(chunkFile,"rw");
+            int len = -1;
+            while((len=raf_read.read(b))!=-1){
+                raf_write.write(b,0,len);
+            }
+            raf_read.close();
+        }
+        raf_write.close();
+    }
+}
